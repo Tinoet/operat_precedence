b940130c63182e62241b30a84bbe75c677bba7ee
Tinoet <88379020+Tinoet@users.noreply.github.com>
2022-03-20 22:44:33 +0100

    Add files via upload

---
+++ operator_precednce/operat_precednce.py
@@ -0,0 +1,5 @@
+# this is the order of oprators in pythons : exponentiation,multiplication,addition or subtraction
+x = 2**3 + 10 *5
+print(x)
+# adding paranthesis will modify it , it comes over all other operators
+
